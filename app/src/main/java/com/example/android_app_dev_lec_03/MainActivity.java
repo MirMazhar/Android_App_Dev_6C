9d683e32ee0e7dbac07919997ee23fa810ac5e78
MirMazhar <mir.mazhar623@gmail.com>
2022-03-18 10:25:46 +0500

    layouts and Picture animation

--- app/src/main/java/com/example/android_app_dev_lec_03/MainActivity.java
+++ app/src/main/java/com/example/android_app_dev_lec_03/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean DisplaySzab =true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnBtnLoginClick(View view){
-        EditText userName = findViewById(R.id.editTextUsername);
-        EditText password = findViewById(R.id.editTextPassword);
+        EditText userName = findViewById(R.id.edittxtusername);
+        EditText password = findViewById(R.id.edittxtpassword);
         Log.i("info", userName.getText().toString());
         Log.i("info", password.getText().toString());
         if( !( userName.getText().toString().equals("mir"))){
@@ -26,5 +28,36 @@ public class MainActivity extends AppCompatActivity {
         if( !(password.getText().toString().equals("123"))){
             Log.i("info", "INCORRECT PASSWORD");
         }
+    }
+
+    public void OnBtnResetClick(View view){
+        EditText userName = findViewById(R.id.edittxtusername);
+        EditText password = findViewById(R.id.edittxtpassword);
+        userName.setText("");
+        password.setText("");
+    }
+
+    public void OnImageClick(View view){
+        ImageView SzabistImage = (ImageView)findViewById(R.id.imgViewzab);
+        ImageView TomImage = (ImageView)findViewById(R.id.imgViewTom);
+
+
+        if(DisplaySzab){
+            SzabistImage.animate().rotation(360).alpha(0).setDuration(3000);
+            TomImage.animate().rotation(360).alpha(1).setDuration(3000);
+            DisplaySzab= false;
+            Toast.makeText(this, "Display Szabist", Toast.LENGTH_LONG).show();
+        }else{
+            SzabistImage.animate().rotation(360).alpha(1).setDuration(3000);
+            TomImage.animate().rotation(360).alpha(0).setDuration(3000);
+            DisplaySzab= true;
+            Toast.makeText(this, "Display Szabist", Toast.LENGTH_LONG).show();
+        }
+//        SzabistImage.animate().alpha(0).setDuration(3000);
+
+//        SzabistImage.setTranslationY(100);
+//        SzabistImage.animate().translationY(100);
+//        SzabistImage.animate().translationYBy(100);
+//        SzabistImage.animate().translationX(100);
     }
 }
