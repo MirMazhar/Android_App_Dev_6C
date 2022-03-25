f06e4f4feb3f7430924b4f015e5346588e2e08f3
MirMazhar <mir.mazhar623@gmail.com>
2022-03-25 09:58:16 +0500

    intent Activity

--- app/src/main/java/com/example/android_app_dev_lec_03/MainActivity.java
+++ app/src/main/java/com/example/android_app_dev_lec_03/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android_app_dev_lec_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+<<<<<<< Updated upstream
 
     public void OnBtnLoginClick(View view){
         EditText userName = findViewById(R.id.editTextUsername);
@@ -27,4 +29,54 @@ public class MainActivity extends AppCompatActivity {
             Log.i("info", "INCORRECT PASSWORD");
         }
     }
+=======
+    public void SwitchActivity(View view){
+        Intent switchActivity = new Intent(this,intentActivity.class);
+        startActivity(switchActivity);
+    }
+
+//    public void OnBtnLoginClick(View view){
+//        EditText userName = findViewById(R.id.edittxtusername);
+//        EditText password = findViewById(R.id.edittxtpassword);
+//        Log.i("info", userName.getText().toString());
+//        Log.i("info", password.getText().toString());
+//        if( !( userName.getText().toString().equals("mir"))){
+//            Log.i("info", "INCORRECT USERNAME");
+//        }
+//        if( !(password.getText().toString().equals("123"))){
+//            Log.i("info", "INCORRECT PASSWORD");
+//        }
+//    }
+//
+//    public void OnBtnResetClick(View view){
+//        EditText userName = findViewById(R.id.edittxtusername);
+//        EditText password = findViewById(R.id.edittxtpassword);
+//        userName.setText("");
+//        password.setText("");
+//    }
+//
+//    public void OnImageClick(View view){
+//        ImageView SzabistImage = (ImageView)findViewById(R.id.imgViewzab);
+//        ImageView TomImage = (ImageView)findViewById(R.id.imgViewTom);
+//
+//
+//        if(DisplaySzab){
+//            SzabistImage.animate().rotation(360).alpha(0).setDuration(3000);
+//            TomImage.animate().rotation(360).alpha(1).setDuration(3000);
+//            DisplaySzab= false;
+//            Toast.makeText(this, "Display Szabist", Toast.LENGTH_LONG).show();
+//        }else{
+//            SzabistImage.animate().rotation(360).alpha(1).setDuration(3000);
+//            TomImage.animate().rotation(360).alpha(0).setDuration(3000);
+//            DisplaySzab= true;
+//            Toast.makeText(this, "Display Szabist", Toast.LENGTH_LONG).show();
+//        }
+////        SzabistImage.animate().alpha(0).setDuration(3000);
+//
+////        SzabistImage.setTranslationY(100);
+////        SzabistImage.animate().translationY(100);
+////        SzabistImage.animate().translationYBy(100);
+////        SzabistImage.animate().translationX(100);
+//    }
+>>>>>>> Stashed changes
 }
