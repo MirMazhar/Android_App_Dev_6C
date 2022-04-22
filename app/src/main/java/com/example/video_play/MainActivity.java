1a9b77a125056c555a158fc18feaaf38699fc375
MirMazhar <mir.mazhar623@gmail.com>
2022-04-22 09:15:39 +0500

    audio

--- app/src/main/java/com/example/video_play/MainActivity.java
+++ app/src/main/java/com/example/video_play/MainActivity.java
@@ -2,24 +2,52 @@ package com.example.video_play;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+    int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        mediaPlayer = MediaPlayer.create(this, R.raw.iphone);
+        SeekBar VolumeControl = findViewById(R.id.seekBar2);
+        VolumeControl.setMax(maxVolume);
 
-        VideoView videoView = (VideoView)findViewById(R.id.videoView);
-        videoView.setVideoPath("android.resources://"+getPackageName()+"/"+R.raw.national);
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
-        videoView.start();
+        VolumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Toast.makeText(getApplicationContext(),Integer.toString(i),Toast.LENGTH_LONG).show();
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
+    public void OnPlay(View view){
+        mediaPlayer.start();
+    }
+    public void OnPause(View view){
+        mediaPlayer.pause();
+    }
 
 }
